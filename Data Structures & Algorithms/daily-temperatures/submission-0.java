c7546adad4902687af131fafe0a80849c0f98479
Haripal Baluja <haripalbaluja.208@gmail.com>
2026-04-29 08:50:37 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] result = new int[length];
+
+        for(int i = 0; i < length; i++) {
+            for(int j = i+1; j < length; j++) {
+                if(temperatures[j] > temperatures[i]) {
+                    result[i] = j-i;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
