6f5f3635ead88e2dee3bac7c3f9b86e68f7bd7f3
Haripal Baluja <haripalbaluja.208@gmail.com>
2026-06-29 09:33:20 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.java
@@ -0,0 +1,47 @@
+class Solution {
+    //private List<String> strs;
+
+    public String encode(List<String> strs) {
+        if(strs.isEmpty()) {
+            return "";
+        }
+        List<Integer> sizes = new ArrayList<>();
+        for(String str : strs) {
+            sizes.add(str.length());
+        }
+        StringBuilder res = new StringBuilder();
+        for(Integer size : sizes) {
+            res.append(size).append(',');
+        }
+        res.append('#');
+        for(String str : strs) {
+            res.append(str);
+        }
+        return res.toString();
+    }
+
+    public List<String> decode(String str) {
+        if(str.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        List<Integer> sizes = new ArrayList<>();
+
+        while(str.charAt(i) != '#') {
+            StringBuilder cur = new StringBuilder();
+            while(str.charAt(i) != ',') {
+                cur.append(str.charAt(i));
+                i++;
+            }
+            sizes.add(Integer.parseInt(cur.toString()));
+            i++;
+        }
+        i++;
+        for(int size : sizes) {
+            result.add(str.substring(i, i+size));
+            i += size;
+        }
+        return result;
+    }
+}
