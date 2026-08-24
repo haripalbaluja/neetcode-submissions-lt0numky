7604ec9f3aa172763c1e725994c6887f736a6de0
Haripal Baluja <haripalbaluja.208@gmail.com>
2026-08-24 10:29:19 +0530

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (ch == ')' && stack.peek() == '(') {
+                    stack.pop();
+                }
+                else if (ch == '}' && stack.peek() == '{') {
+                    stack.pop();
+                }
+                else if (ch == ']' && stack.peek() == '[') {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
